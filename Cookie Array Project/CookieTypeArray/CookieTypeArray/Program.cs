bf05220a76cd234ca2eaea223b8a29e90050d893
Jermadillo <56244087+Jermadillo@users.noreply.github.com>
2019-11-13 22:14:43 -0800

    **Last working session**

--- Cookie Array Project/CookieTypeArray/CookieTypeArray/Program.cs
+++ Cookie Array Project/CookieTypeArray/CookieTypeArray/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CookieTypeArray
 {
@@ -6,7 +7,36 @@ namespace CookieTypeArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //This is my cookie array project to help me get better at validating and dealing with arrays
+
+            Console.WriteLine("We are going to make some money today with a cookie business.");
+            Console.WriteLine("First we need to know how many cookie types you want to sell");
+
+            int howMany = Validate.ValidateInt(Console.ReadLine());
+
+            Console.WriteLine("So you want to sell {0} different types of cookies",howMany);
+
+            Console.WriteLine("please type in the names of the different cookie types seperated by commas");
+            string cookieTypeString = Validate.ValidateString(Console.ReadLine());
+
+
+
+
+            string [] cookieTypeArray = cookieTypeString.Split(",");
+            
+
+            for (int i = 0; i < cookieTypeArray.Length; i++)
+            {
+                
+                Console.WriteLine(cookieTypeArray[i]);
+            }
+
+
+
+
+
+
         }
     }
 }
---
+++ Cookie Array Project/CookieTypeArray/CookieTypeArray/Validate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace CookieTypeArray
+{
+    public class Validate
+    {
+
+
+
+        public static string ValidateString(string UserI)
+        {
+            while (string.IsNullOrWhiteSpace(UserI))    
+            {
+                Console.WriteLine("Don't leave this blank please");
+                UserI = Console.ReadLine();
+            }
+
+            UserI = UserI.Trim();
+
+            return UserI;
+        }
+
+
+        public static int ValidateInt (string UserI)
+        {
+            int howMany;
+
+            while (!(int.TryParse(UserI,out howMany)))
+            {
+                Console.WriteLine("Please only select a whole number");
+                UserI = Console.ReadLine();
+
+            }
+
+            return howMany;
+
+        }
+
+
+
+
+    }
+}
