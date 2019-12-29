7a21bb5555dbe19bb9d73f46eea0399bcff9e874
Jermadillo <56244087+Jermadillo@users.noreply.github.com>
2019-12-28 20:08:53 -0800

    **Completed files that needed to be finalized and updated**

---
+++ StringObjectsProject/StringObjectsProject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StringObjectsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //this is a program called credti card observer
+            //the code is supposed to return back a number
+            //the person types in so that all but the last 4 numbers are X's
+            //The program needs to count how long it is and replace all but 4 numbers with X's
+
+
+            Console.WriteLine("Enter in a number from 12 - 16 characters long");
+
+            string userNum = Validate.ValidateString(Console.ReadLine());
+
+            Console.WriteLine("You chose this {0} as your number",userNum);
+
+            char[] arrayNum = new char[userNum.Length];
+
+
+
+            userNum.ToCharArray(0,arrayNum.Length);
+
+
+            for (int i = 0; i < arrayNum.Length; i++)
+            {
+                Console.WriteLine(arrayNum[i]);
+            }
+
+        }
+    }
+}
---
+++ StringObjectsProject/StringObjectsProject/Validate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace StringObjectsProject
+{
+    public class Validate
+    {
+        public static string ValidateString( string UserI)
+        {
+
+            while (string.IsNullOrEmpty(UserI) && UserI.Length == 12 && UserI.Length <= 16) 
+            {
+                Console.WriteLine("Please don't leave this blank\r\n Make sure it is between 12 and 16 long");
+                UserI = Console.ReadLine();
+            }
+
+            return UserI;
+        }
+
+
+
+
+
+
+
+    }
+}
